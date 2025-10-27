2fcdb6b77e4cc1af2eb5841e11674bc8a1849c36
Jacritt <joshua10154799@gmail.com>
2025-10-27 14:26:24 -0400

    login to home
    Added scene changer and a button from login to the main game

---
+++ Assets/Scripts/Scene switcher.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneswitcher : MonoBehaviour
+{
+    public static int selectedLevel;
+    public int level;
+
+    public void LoadScene(string sceneName)
+    {
+        selectedLevel = level;
+        SceneManager.LoadScene(sceneName);
+    }
+}
